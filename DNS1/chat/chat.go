2b94082fbacd8d380a364b22d30d8fdb5140f82a
javiermartinez27 <50684376+javiermartinez27@users.noreply.github.com>
2020-12-23 17:14:17 -0300

    Se crea el registro con el create

--- DNS1/chat/chat.go
+++ DNS1/chat/chat.go
@@ -2,6 +2,8 @@ package chat
 
 import (
 	"log"
+	"os"
+	"strings"
 
 	"golang.org/x/net/context"
 )
@@ -11,8 +13,28 @@ var ipActual int = 1
 type Server struct {
 }
 
+func crearRegistro(registro string) {
+	registroSeparado := strings.Split(registro, ".")
+	nombre := "registros_zf/registro_" + registroSeparado[1] + ".txt"
+	f, err := os.OpenFile(nombre, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err2 := f.WriteString(registro + "\n")
+	if err2 != nil {
+		log.Fatal(err)
+	}
+}
+
 func (s *Server) RecibirDeAdmin(ctx context.Context, in *Message) (*Message, error) { //cuando un admin envia una peticion
 	log.Printf("Cliente envía etición: %s", in.Mensaje)
-	//HACER ALGO
+	separar := strings.Split(in.Mensaje, " ")
+	if separar[0] == "create" {
+		crearRegistro(separar[1])
+	} else if separar[0] == "update" {
+		//updateregistro(separar[1], separar[2])
+	} else {
+		//borrarregistro(separar[1])
+	}
 	return &Message{Mensaje: "Recibido"}, nil
 }
